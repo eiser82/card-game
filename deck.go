1396aa46d648414981db7f3bcc10046b397a9d9f
Ernesto Iser <ernesto.iser@networkedinsights.com>
2020-03-31 08:12:59 -0500

    make a full deck of cards

--- deck.go
+++ deck.go
@@ -16,8 +16,26 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	cardSuits := []string{
+		"Spades",
+		"Diamonds",
+		"Hearts",
+		"Clubs"}
+
+	cardValues := []string{
+		"Ace",
+		"Two",
+		"Three",
+		"Four",
+		"Five",
+		"Six",
+		"Seven",
+		"Eight",
+		"Nine",
+		"Ten",
+		"Jack",
+		"Queen",
+		"King"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
